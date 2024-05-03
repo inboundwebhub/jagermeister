c36fa379e86cd0189fb23921104707a64bf97dbd
Jaydip <jaydeep.inboundwebhub@gmail.com>
2024-05-03 10:41:54 +0530

    add icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
     <title> {{ config('app.name', 'Laravel') }} |  {{ __($pageTitle ?? __('')) }} </title>
+    <link rel="icon" href="{{ asset('assets/images/apple-touch-icon.webp') }}">
+    
     <meta content="Admin Dashboard" name="description" />
     <meta content="ThemeDesign" name="author" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
